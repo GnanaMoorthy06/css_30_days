6525e146f9787d0462d83553d855a4ba63544864
GNANA MOORTHY D <129540732+GnanaMoorthy06@users.noreply.github.com>
2026-04-12 03:08:33 +0530

    css colors and unit

---
+++ day5/concepts.js
@@ -0,0 +1,15 @@
+/* 
+
+CSS UNITS
+---------
+
+px : absolute units  => no parents / what we give it will replect
+
+%  : relative units  => depents upon parent element 
+
+rem : relative units  => depents upon root element | for font => root font size X 2 = ?
+
+em : relative units  => depents upon emittied parent element | for font => parent font size X 2 = ?
+
+
+*/
